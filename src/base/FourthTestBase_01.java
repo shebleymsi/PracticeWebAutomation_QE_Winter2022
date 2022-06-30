e8ddad1e16a1a2add8ea279ed60510e76b79df34
Shebleymsi <Shebley2022@gmail.com>
2022-06-30 01:32:05 -0400

    Fourth_Reserve

---
+++ src/base/FourthTestBase_01.java
@@ -0,0 +1,52 @@
+package base;
+
+public class FourthTestBase_01 {
+//    // Configuration purpose
+//    public static WebDriver driver;
+//    String browserName = "chrome";
+//    String url = "https://www.amazon.com/";
+//
+//    @BeforeTest
+//    public void setUpAutomation() {
+//        System.out.println("***************** Automation Started *******************");
+//    }
+//
+//    @AfterTest(alwaysRun = true)
+//    public void tearDownAutomation() {
+//        driver.close();
+//        System.out.println("***************** Automation End *******************");
+//    }
+//
+//    @BeforeTest
+//    public void setUp() {
+//        if (this.browserName == "chrome") {
+//            setUpChromeBrowser();
+//        } else if (this.browserName == "fireFox") {
+//            setUpFireFoxBrowser();
+//        }
+//        //  navigate to amazon.com
+//        driver.manage().window().maximize();
+//        driver.get(this.url);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+//        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+//        driver.manage().deleteAllCookies();
+//    }
+//
+//    public static void setUpChromeBrowser() {
+//        String chromeDriverPath = "../PracticeWebAutomation_QE_Winter2022/BrowserDriver/Windows/chromedriver.exe";
+//        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+//        driver = new ChromeDriver();
+//    }
+//
+//    public static void setUpFireFoxBrowser() {
+//        String fireFoxDriverPath = "../PracticeWebAutomation_QE_Winter2022/BrowserDriver/Windows/geckodriver.exe";
+//        System.setProperty("webdriver.gecko.driver", fireFoxDriverPath);
+//        driver = new FirefoxDriver();
+//    }
+//
+//    public static void display() {
+//        System.out.println("This is display");
+//    }
+
+
+}
---
+++ src/testAmazon/FourthAmazonHomePageTest_01.java
@@ -0,0 +1,35 @@
+package testAmazon;
+
+import base.FourthTestBase_01;
+
+public class FourthAmazonHomePageTest_01 extends FourthTestBase_01 {
+//
+//    // Test class
+//
+//    @Test
+//    public void testNavigateToYahoo() throws InterruptedException {
+//        navigateToYahoo();
+//    }
+//
+//    @Test
+//    public void testSearchBoxOnChrome() throws InterruptedException {
+//        searchBoxOnChrome();
+//    }
+//
+//    @Test
+//    public void testSearchBoxOnFireFox() throws InterruptedException {
+//        searchBoxOnFireFox();
+//    }
+//
+//    @Test
+//    public void testSignIn() throws InterruptedException {
+//        signIn();
+//    }
+//
+//    @Test(enabled = false)
+//    public void testSignInUsingInvalidCredential() throws InterruptedException {
+//        signInUsingInvalidCredential();
+//    }
+
+
+}
